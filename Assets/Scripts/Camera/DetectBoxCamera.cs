19e8c53e05c601880bea4e231856da4c03c5293b
MarcRuizenti <146344001+MarcRuizenti@users.noreply.github.com>
2024-02-06 10:58:42 +0100

    camara perfecta 2.0

--- Assets/Scripts/Camera/DetectBoxCamera.cs
+++ Assets/Scripts/Camera/DetectBoxCamera.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DetectBoxCamera : MonoBehaviour
@@ -8,38 +9,46 @@ public class DetectBoxCamera : MonoBehaviour
     private float _vertical;
     [SerializeField] private GameObject _boxCamera;
     [SerializeField] private float _cameraSpeed;
+    private Rigidbody2D _rb;
 
-
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
     private void Update()
     {
         Vector3 origin = _boxCamera.transform.position;
         Vector3 newPosition = _boxCamera.transform.position + new Vector3(_horizontal, _vertical, 0);
 
-        if (Vector3.Distance(_boxCamera.transform.position, transform.position) < -9 || Vector3.Distance(_boxCamera.transform.position, transform.position) > 9)
+        if (Vector3.Distance(_boxCamera.transform.position, transform.position) < -9 || Vector3.Distance(_boxCamera.transform.position, transform.position) > 9 || _boxCamera.transform.position.y - transform.position.y > 4)
         {
             newPosition = transform.position;
+         
+            _boxCamera.transform.position = Vector3.Lerp(origin, newPosition, Time.deltaTime * (_cameraSpeed + Vector3.Distance(_boxCamera.transform.position, transform.position)));
         }
-
-        _boxCamera.transform.position = Vector3.Lerp(origin, newPosition, Time.deltaTime * _cameraSpeed);
+        else
+        {
+            _boxCamera.transform.position = Vector3.Lerp(origin, newPosition, Time.deltaTime * _cameraSpeed);
+        }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Derecha"))
         {
-            _horizontal = 0.5f;
+            _horizontal = 1f;
         }
         if (collision.CompareTag("Izquierda"))
         {
-            _horizontal = -0.5f;
+            _horizontal = -1f;
         }
         if (collision.CompareTag("Arriba"))
         {
-            _vertical = 0.5f;
+            _vertical = 1f;
         }
         if (collision.CompareTag("Abajo"))
         {
-            _vertical = -0.5f;
+            _vertical = -1f;
         }
     }
 
