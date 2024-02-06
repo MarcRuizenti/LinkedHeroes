047117ea3d14c3a75e4a10f7dc61b1b6e240b673
albertodt <113683638+abertodt@users.noreply.github.com>
2024-02-06 11:43:11 +0100

    Gancho se desactiva cuando estamos en el suelo

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -5,10 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject _player;
     public static GameManager Instance { get; private set; }
 
     public int Health;
     public bool PlayerDeath;
+
+
+    public enum Character
+    {
+        AIKE,
+        KROKUR
+    }
+
+    public Character _currentCharacter;
 
     private void Awake()
     {
@@ -24,17 +34,16 @@ public class GameManager : MonoBehaviour
         }
     }
 
-
-
     // Start is called before the first frame update
     void Start()
     {
-        
+        _currentCharacter = Character.AIKE;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //UpdateCharacter(_currentCharacter);
         if(PlayerDeath && Input.GetKeyUp(KeyCode.Escape)) {
             Respawn();
         }
@@ -55,5 +64,35 @@ public class GameManager : MonoBehaviour
     private void Respawn()
     {
         SceneManager.LoadScene("SampleScene");
+    }
+
+    private void UpdateCharacter(Character _character)
+    {
+        switch(_character)
+        {
+            case Character.AIKE:
+                _player.GetComponent<SpriteRenderer>().color = Color.green;
+                break; 
+            case Character.KROKUR:
+                _player.GetComponent<SpriteRenderer>().color = Color.cyan;
+                break;
+        }
+    }
+
+    public void ChangeCharacter()
+    {
+        switch (_currentCharacter)
+        {
+            case Character.KROKUR:
+                _player.GetComponent<SpriteRenderer>().color = Color.green;
+                _currentCharacter = Character.AIKE;
+                break;
+            case Character.AIKE:
+                _player.GetComponent<SpriteRenderer>().color = Color.cyan;
+                _currentCharacter = Character.KROKUR;
+                break;
+            default:
+                break;
+        }
     }
 }
