fa95c68d28c808e7654457696857a4554fc9750c
albertodt <113683638+abertodt@users.noreply.github.com>
2024-02-20 11:55:55 +0100

    Menús añadidos
    Menu principal, opciones y pausa

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    [SerializeField] private bool _isAdditive;
+    public void ExitGame()
+    {
+        Debug.Log("Adios");
+        Application.Quit();
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadSceneAditive(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+        Scene activeScene = SceneManager.GetActiveScene();
+    }
+
+    private void Update()
+    {
+        //if(_isAdditive &&)
+    }
+}
