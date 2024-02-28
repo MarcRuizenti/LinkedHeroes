55d909f2b9d40f3cef1e1c9ccb1c71aca3d8b91a
JuanCarlosHeras <juancarlos.heras@enti.cat>
2024-02-28 19:37:18 +0100

    Título
    fondo con título

---
+++ Assets/Scripts/background/BackgroundLines.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundLines : MonoBehaviour
+{
+
+    [SerializeField] float velocityY;
+
+    float spawnPosY = 6f; //6.24f
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + velocityY * Time.deltaTime, this.transform.position.z);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger");
+        if (!collision.CompareTag("BackgroundTrigger"))
+            return;
+        this.transform.position = new Vector3(this.transform.position.x, spawnPosY, this.transform.position.z);
+
+    }
+}
