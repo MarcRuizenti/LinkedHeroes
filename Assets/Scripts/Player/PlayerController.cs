ea28eaa48052637e0cd5e5c62de7358592589127
MarcRuizenti <146344001+MarcRuizenti@users.noreply.github.com>
2024-04-22 11:17:49 +0200

    a

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -54,7 +54,7 @@ public class PlayerController : MonoBehaviour
     public List<Vector3> _origins;
     [SerializeField] private LayerMask _groundLayer;
     
-    public LineRenderer _lineRenderer;
+    LineRenderer _lineRenderer;
     DistanceJoint2D _distanceJoint;
 
     //hook variables
