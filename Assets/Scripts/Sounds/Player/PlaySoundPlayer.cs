0561c1f06d94b456f3c3d32937f73c355b608b08
MarcRuizenti <marc.ruiz.garcia@enti.cat>
2024-04-30 17:20:59 +0200

    sonidos Stone Grass Ice

--- Assets/Scripts/Sounds/Player/PlaySoundPlayer.cs
+++ Assets/Scripts/Sounds/Player/PlaySoundPlayer.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlaySoundPlayer : MonoBehaviour
 {
     public AudioClip[] stepsGrass;
+    public AudioClip[] stepsIce;
+    public AudioClip[] stepsStone;
     public PlayerController controller;
 
     private void Start()
@@ -13,7 +15,25 @@ public class PlaySoundPlayer : MonoBehaviour
     }
     public void PlaySteps()
     {
-        AudioClip stepGrass = stepsGrass[Random.Range(0, stepsGrass.Length)];
-        SoundManager.Instance.EjecutarAudio(stepGrass, 1, 0.1f);
+        switch (controller.matirial)
+        {
+            case PlayerController.Matirials.ICE:
+                AudioClip stepIce = stepsIce[Random.Range(0, stepsIce.Length)];
+                SoundManager.Instance.EjecutarAudio(stepIce, 1, 0.2f);
+                break;
+            case PlayerController.Matirials.STONE:
+                AudioClip stepStone = stepsStone[Random.Range(0, stepsStone.Length)];
+                SoundManager.Instance.EjecutarAudio(stepStone, 1, 0.1f);
+                break;
+            case PlayerController.Matirials.GRASS:
+                AudioClip stepGrass = stepsGrass[Random.Range(0, stepsGrass.Length)];
+                SoundManager.Instance.EjecutarAudio(stepGrass, 1, 0.1f);
+                break;
+            default:
+                AudioClip stepGrass1 = stepsGrass[Random.Range(0, stepsGrass.Length)];
+                SoundManager.Instance.EjecutarAudio(stepGrass1, 1, 0.1f);
+                break;
+        }
+
     }
 }
